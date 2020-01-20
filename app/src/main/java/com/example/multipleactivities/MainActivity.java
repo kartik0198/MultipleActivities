c8f52767be35a26bf8a10284f387ce709d057bd0
Kartik Singhal <kartiksinghal0198@gmail.com>
2020-01-20 12:08:45 +0530

    Added a new toast. :wq

--- app/src/main/java/com/example/multipleactivities/MainActivity.java
+++ app/src/main/java/com/example/multipleactivities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 startActivity(intent);
             }
